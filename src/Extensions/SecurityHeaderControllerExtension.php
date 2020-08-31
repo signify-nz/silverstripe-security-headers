3c19aebfa86004ce6cf31acccb24ff3ae7b1e181
Guy Sartorelli <guy.sartorelli@signify.co.nz>
2020-08-31 12:31:40 +1200

    feat: Add or alter the report-uri CSP directive.
    If a user-defined report-uri is specified, add the endpoint for our CMSViolationsController as well.
    If no user-defined report-uri is specified, only add the CMS report URI.

--- src/Extensions/SecurityHeaderControllerExtension.php
+++ src/Extensions/SecurityHeaderControllerExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Signify\Extensions;
+namespace Guttmann\SilverStripe\Extensions;
 
 use SilverStripe\Core\Extension;
 use SilverStripe\Core\Config\Configurable;
@@ -16,6 +16,14 @@ class SecurityHeaderControllerExtension extends Extension
      */
     private static $headers;
 
+    /**
+     * The URI to report CSP violations to.
+     * See routes.yml
+     * @config
+     * @var string
+     */
+    private static $report_uri;
+
     public function onAfterInit()
     {
         $response = $this->owner->getResponse();
@@ -23,8 +31,32 @@ class SecurityHeaderControllerExtension extends Extension
         $headersToSend = (array) $this->config()->get('headers');
 
         foreach ($headersToSend as $header => $value) {
+            // Add the report-uri directive.
+            // TODO add or amend report-to directive and Report-To header.
+            // SEE https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/report-to
+            // SEE https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy/report-uri for report-uri deprecation
+            // SEE https://canhas.report/csp-report-to
+            if ($header === 'Content-Security-Policy') {
+                $value = preg_replace('/\v/', '', $value);
+                if (strpos($value, 'report-uri')) {
+                    $value = str_replace('report-uri', "report-uri {$this->getReportURI()}", $value);
+                } else {
+                    $value = rtrim($value, ';') . "; {$this->getReportURIDirective()}";
+                }
+            }
+
             $response->addHeader($header, $value);
         }
+    }
+
+    protected function getReportURI()
+    {
+        return $this->config()->get('report_uri');
+    }
+
+    protected function getReportURIDirective()
+    {
+        return "report-uri {$this->getReportURI()};";
     }
 
 }
