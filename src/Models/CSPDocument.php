7f4ff576fc49a2bad939fdc9ca692b33c9ffd7e2
Owen Walker <owen.walker@signify.co.nz>
2026-07-24 14:18:17 +1200

    fix: add missing get_or_create() method. SIGIMM-72

--- src/Models/CSPDocument.php
+++ src/Models/CSPDocument.php
@@ -2,8 +2,10 @@
 
 namespace Signify\SecurityHeaders\Models;
 
+use SilverStripe\CMS\Controllers\RootURLController;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Control\Director;
 
 class CSPDocument extends DataObject
 {
@@ -20,4 +22,21 @@ class CSPDocument extends DataObject
     private static $belongs_many_many = [
         'CSPViolations' => CSPViolation::class,
     ];
+
+    public static function get_or_create(string $documentURI): CSPDocument
+    {
+        $document = self::get()->filter(['URI' => $documentURI])->first();
+        if (!$document) {
+            $document = self::create(['URI' => $documentURI]);
+            $siteTreeLink = $documentURI;
+            if (!Director::makeRelative($siteTreeLink)) {
+                $siteTreeLink = RootURLController::get_homepage_link();
+            }
+            if ($siteTree = SiteTree::get_by_link($siteTreeLink)) {
+                $document->SiteTreeID = $siteTree->ID;
+            }
+            $document->write();
+        }
+        return $document;
+    }
 }
